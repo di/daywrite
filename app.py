e0ab599add504506080d8630ea3a51c38dc66b5c
Dustin Ingram <dustin.ingram@gmail.com>
2014-05-09 13:32:56 +0000

    Modified to handle heroku port

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ else :
 if __name__ == '__main__' :
     app = sevenfiftyone.app
     app.db = db
-    app.run(host="0.0.0.0", port=80, debug=True, use_debugger=True, use_reloader=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, debug=True, use_debugger=True, use_reloader=True)
