a24d3556c6874f1d04a28824d4209c68dc501235
Dustin Ingram <dustin.ingram@gmail.com>
2014-09-26 16:33:58 -0400

    Scroll bug with multiple scrollbacks fixed

--- sevenfiftyone/static/751.js
+++ sevenfiftyone/static/751.js
@@ -14,8 +14,8 @@ jQuery(function($) {
         options: {
           type: "POST",
           success: function() {
-            jQuery("#current").animate({"border-color": "#8CC665"}, 1000)
-            jQuery("#current").animate({"border-color": "#EEEEEE"}, 3000)
+            jQuery("#current").animate({"color": "#1abc9c"}, 1000)
+            jQuery("#current").animate({"color": "#becac9"}, 3000)
           },
           error: function() {
             alert("error!");
@@ -30,7 +30,7 @@ var scrollToBottom = function(){
     // Check if the caret is at the end of the textarea
     if ($('textarea').caret() == $('textarea').val().length) {
         // If it is, do the animation
-        $('html, body').animate({scrollTop: $(document).height()}, 'slow');
+        $('html, body').stop().animate({scrollTop: $(document).height()});
     }
 };
 
