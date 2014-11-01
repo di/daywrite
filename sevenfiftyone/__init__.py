2ac2090b11aae60adc5267761e8a74ff24015eea
Dustin Ingram <dustin.ingram@gmail.com>
2014-11-01 16:40:02 -0400

    Fixes #3, also some cleanup

--- sevenfiftyone/__init__.py
+++ sevenfiftyone/__init__.py
@@ -26,10 +26,16 @@ class AnonymousUser(AnonymousUserMixin):
     self.admin = False
 
 def make_datestring(date):
+    ''' Takes in a date and makes it into the url-valid datestring. '''
     return date.strftime("%Y-%m-%d")
 
 def make_text_date(date):
+    ''' Takes in a date and makes it a human-readable datestring. '''
     return date.strftime("%b %-d, %Y")
+
+def today():
+    ''' Returns the current date for the current user. '''
+    return datetime.now(timezone(current_user.timezone))
 
 app = Flask(__name__)
 
@@ -45,17 +51,16 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 punct = re.compile("\.|!|\?|;|:|,")
 
 def past_status():
-    date = datetime.now(timezone(current_user.timezone))
     post_map = dict()
     posts = Post.objects(owner=current_user.id).order_by('-date').limit(30)
     for post in posts:
         post_map[post.date_string] = (post.completed, post.length, post.url_string())
-    return [post_map.get(make_datestring(date - timedelta(days=i)), (False, 0, None)) for i in reversed(range(1,30))]
+    return [post_map.get(make_datestring(today() - timedelta(days=i)), (False, 0, None)) for i in reversed(range(1,30))]
 
 @app.route("/", methods=["GET"])
 def index():
     if current_user.is_authenticated():
-        date_string = make_datestring(datetime.now(timezone(current_user.timezone)))
+        date_string = make_datestring(today())
         post, created = Post.objects.get_or_create(date_string=date_string, owner=current_user.id)
         return render_template('index.html', post=post, past_status=past_status(), user=current_user)
     return render_template('login.html', form=LoginForm(), ref=request.args.get('next', None))
@@ -64,7 +69,7 @@ def index():
 def post_root():
     if current_user.is_authenticated():
         content = request.form["content"]
-        date_string = make_datestring(datetime.now(timezone(current_user.timezone)))
+        date_string = make_datestring(today())
 
         # Make sure the post is for the current day
         if request.form["date_string"] == date_string:
@@ -92,7 +97,8 @@ def post_root():
                 blurb = blurb[:blurb.rfind(" ")] + "..."
 
             # Save the post
-            Post.objects.get(date_string=date_string, owner=current_user.id).update(set__content=content, set__completed=completed, set__length=length, set__blurb=blurb)
+            post = Post.objects.get(date_string=date_string, owner=current_user.id)
+            post.update(set__content=content, set__completed=completed, set__length=length, set__blurb=blurb)
 
             # Check the user's archive flag
             if not current_user.has_archive:
@@ -116,18 +122,27 @@ def post_root():
 @app.route("/<int:year>/<int:month>/<int:day>/", methods=["GET"])
 def get_post(year, month, day):
     if current_user.is_authenticated():
+        # Get the date string with leading zeros from the URL
         date_string = "%d-%02d-%02d" % (year, month, day)
+
+        # If the archive is today, go to the index instead
+        if date_string == make_datestring(today()):
+            return redirect(url_for("index"))
+
+        # See if the date in the URL corresponds to a post
         try:
             post = Post.objects.get(date_string=date_string, owner=current_user.id)
         except Post.DoesNotExist:
             return render_template('missing.html', date_string=date_string)
+
         return render_template('past_single.html', post=post)
     return redirect(url_for("index"))
 
 @app.route("/past/", methods=["GET"])
 def get_past():
     if current_user.is_authenticated():
-        return render_template('past.html', posts=Post.objects(owner=current_user.id, length__gt=0).order_by('-date'))
+        posts = Post.objects(owner=current_user.id, length__gt=0).order_by('-date')
+        return render_template('past.html', posts=posts)
     return redirect(url_for("index"))
 
 @app.route("/register/", methods=["GET"])
