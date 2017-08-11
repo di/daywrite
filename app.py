20be9f4e45d3b44f0583529b554fcfb5003344a5
Dustin Ingram <di@users.noreply.github.com>
2017-08-11 10:53:39 -0400

    Use MONGODB_URI instead

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 import os
-from flask import Flask
 from urlparse import urlparse
-from flask import Flask
-from pymongo import Connection
 import sevenfiftyone
-from mongoengine import *
 from flask.ext.mongoengine import MongoEngine
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     app = sevenfiftyone.app
 
     # MongoEngine configuration
     app.config["MONGODB_SETTINGS"] = {
-        "DB": urlparse('MONGOHQ_URL').path[1:],
-        "host": os.environ.get('MONGOHQ_URL')}
+        "DB": urlparse('MONGODB_URI').path[1:],
+        "host": os.environ.get('MONGODB_URI')}
 
     # MongoEngine DB
     db = MongoEngine(app)
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host="0.0.0.0", port=port, debug=True, use_debugger=True, use_reloader=True)
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=True,
+        use_debugger=True,
+        use_reloader=True,
+    )
